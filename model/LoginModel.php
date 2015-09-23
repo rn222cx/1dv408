ca54651b4af1fa6108e7e7a72d09c837e6bf5cfc
Roy Nilsson <rn222cx@student.lnu.se>
2015-09-23 18:21:34 +0200

    remove comment

--- model/LoginModel.php
+++ model/LoginModel.php
@@ -36,12 +36,6 @@ class LoginModel {
         return isset($_SESSION[self::$setSessionUser]);
     }
 
-//    public function forceLogout($message)
-//    {
-//        $_SESSION[self::$sessionLoginMessage] = $message;
-//        $_SESSION[ self::$setSessionUser ] = false;
-//    }
-
     public function destroySession($message){
         unset($_SESSION[self::$setSessionUser]);
         $_SESSION[self::$sessionLoginMessage] = $message;
